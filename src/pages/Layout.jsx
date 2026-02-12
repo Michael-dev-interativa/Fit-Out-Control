a37ce2ece9c374cdc7358b57572affcf76a630d4
Michael Barros <michaelbarros.353@GMAIL.COM>
2026-02-12 16:23:47 -0300

    fix: Remover chaves extras e restaurar imports iniciais em Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,5 @@
-  }
-
-}
-
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { Auth, User } from "@/api/entities";
 import ApiConnectionAlert from "@/components/ApiConnectionAlert";
